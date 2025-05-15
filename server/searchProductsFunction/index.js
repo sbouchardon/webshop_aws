36b74bdbe5d819441bc0911ab7627080cea64ad6
Stephen BOUCHARDON <stephenbouchardon@Stephens-MacBook-Pro.local>
2025-05-15 21:19:12 +0200

    test modif api gateway

--- server/searchProductsFunction/index.js
+++ server/searchProductsFunction/index.js
@@ -1,27 +1,44 @@
 const { Pool } = require('pg');
 
-// Fill in your RDS info here:
 const pool = new Pool({
   user: 'postgres',
   host: 'webshop-db.cafgo66wipxz.us-east-1.rds.amazonaws.com',
-  database: 'webshop-db',
+  database: 'postgres',
   password: 'Pablo24fr',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 exports.handler = async (event) => {
-  try {
-    const result = await pool.query('SELECT * FROM Product');
-    console.log("Fetched products:", result.rows);
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
+      headers,
+      body: 'OK'
+    };
+  }
+
+  try {
+    const result = await pool.query('SELECT * FROM Product');
+    return {
+      statusCode: 200,
+      headers,
       body: JSON.stringify(result.rows)
     };
   } catch (err) {
     console.error('Error querying database:', err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Database error', message: err.message }),
+      headers,
+      body: JSON.stringify({ error: 'Database error', message: err.message })
     };
   }
 };
